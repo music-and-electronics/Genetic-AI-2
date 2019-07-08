e204f5980eb8f92041a4f851b5b537019ee54ff3
김종현 <kjh970407@naver.com>
2019-07-08 13:39:58 +0900

    hello

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+
+namespace Genetic_AI_Demo
+{
+    class Genetic_Json_Data
+    {
+        JObject json_object;
+        string raw_json_data;
+
+        public Genetic_Json_Data()
+        {
+            raw_json_data = File.ReadAllText("C:/Users/kjh97/source/repos/Genetic AI Demo/Genetic_Data.json");
+            json_object = JObject.Parse(raw_json_data);
+        }      
+
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Genetic_Json_Data Genetic_Json_Data = new Genetic_Json_Data();
+            
+        }
+    }
+}
